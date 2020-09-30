e39ee2ffa187a4ec7b68ec882dc3627ef9d8c532
marantaaa <estelaluisoliveraa@gmail.com>
2020-09-30 15:16:27 -0500

    Este es mi primer Script. Siento que ya no tengo uñas y cabello

---
+++ MiPrimerScript.R
@@ -0,0 +1,8 @@
+
+#vamos a crear un vector que contenga cinco variables cuantitativas discretas 
+#contenidas en una variable llamada muestra 
+
+muestra<-c(10,35,20,18,15)
+
+#vamos a calcular la media
+mean(muestra)
